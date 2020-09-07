c069ae1c757423b075e32bf701d89663e398fd1c
Usha G <Usha.G@microchip.com>
2020-09-07 23:26:12 +0530

    MPAE-3361 Adding the updated firmware

---
+++ avr128da48-tcb-frequency-dutycycle-measurement-studio/avr128da48-tcb-frequency-dutycycle-measurement-studio/application.h
@@ -0,0 +1,27 @@
+/*
+ * application.h
+ *
+ * Created: 3/12/2020 10:36:42 PM
+ *  Author: I51005
+ */
+
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+
+/**
+ * **********************************************************
+ *  Macro declaration
+ * **********************************************************
+ **/
+
+#define MAXSAMPLES_COUNT 10
+
+/**
+ * **********************************************************
+ *  function declaration
+ * **********************************************************
+ **/
+
+void FrequencyAndDutycycleMeasurement(void);
+
+#endif /* APPLICATION_H_ */
